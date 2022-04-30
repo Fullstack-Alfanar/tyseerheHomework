9a6e7e1f145f059f8804bdde706c9cd4e4b30375
tyseer <tyseerhe@gmail.com>
2022-04-30 13:13:17 +0300

    changed some files

--- newhomeworks/js/storage.js
+++ newhomeworks/js/storage.js
@@ -1,57 +1,44 @@
+//question 1
 let saveit = document.getElementById("save");
 let loadit = document.getElementById("load");
 let mail = document.getElementById("email");
+
+saveit.addEventListener("click", ()=>{
+    document.cookie = mail.value;
+    console.log(document.cookie);
+}); //mysave
+
+loadit.addEventListener("click", ()=>{
+    alert(mail.value);
+}); //myload
+
 //-----------------------------------------
-let saveit1 = document.getElementById("save1");
+//question 2
+let saveit1 = document.getElementById("save1"); 
 let loadit1 = document.getElementById("load1");
 let Phone = document.getElementById("tel");
+
+saveit1.addEventListener("click", ()=>{
+    document.cookie = Phone.value;
+    console.log(document.cookie);
+}); //mysave1
+
+loadit1.addEventListener("click", ()=>{
+    alert(Phone.value);
+}); //myload1
+
 //------------------------------------------
+
+//question 3
 let myname = document.getElementById("name");
 let Phone1 = document.getElementById("mytel");
 let mail1 = document.getElementById("myEmail");
 let btn = document.getElementById("save2");
-//--------------------------------------------------------------------
-let txt = document.getElementById("text");
-let btn1 = document.getElementById("save3");
-let btn2 = document.getElementById("search");
-//--------------------------------------------------------------------
-saveit.addEventListener("click", mysave);
-loadit.addEventListener("click", myload);
-//------------------------------------------
-saveit1.addEventListener("click", mysave1);
-loadit1.addEventListener("click", myload1);
-//-------------------------------------------
-btn.addEventListener("click", mysave2);
-//-------------------------------------------
-btn1.addEventListener("click", mysave3);
-btn2.addEventListener("click", searching);
 
-function mysave() { //for question 1
-    document.cookie = mail.value;
-    console.log(document.cookie);
-
-}
-
-function myload() {//for question 1
-
-    alert(mail.value);
-}
-//------------------------------------------
-
-function mysave1() { //for question 2
-    document.cookie = Phone.value;
-    console.log(document.cookie);
-}
-
-function myload1() {//for question 2
-    alert(Phone.value);
-}
-
-//-----------------------------------------
-//question 3
 let arr = typeof Array;
 let Name = "";
 let phone;
+
 if (localStorage.getItem("myData") != null) {
     arr = JSON.parse(localStorage.getItem("myData"));
 }
@@ -59,7 +46,7 @@ else {
     arr = [];
 }
 
-function mysave2() {//for question 3
+btn.addEventListener("click", ()=>{
     if (
         myname.value.length > 2 && myname.value.length < 30 ||
         Phone1.value.length > 2 && Phone1.value.length < 30 ||
@@ -82,18 +69,26 @@ function mysave2() {//for question 3
     else {
         alert("it has to be between 2 and 30!");
     }
-}
+
+}); //mysave2
+
+//--------------------------------------------------------------------
 
 //question 4
+let txt = document.getElementById("text");
+let btn1 = document.getElementById("save3");
+let btn2 = document.getElementById("search");
+
 let arr1 = [];
-function mysave3() {
+btn1.addEventListener("click", ()=>{
     if (txt.value != "" || txt.value != null) {
         arr1.push(txt.value);
         localStorage.setItem("local", JSON.stringify(arr1));
     }
-}
 
-function searching() {
+}); //mysave3
+
+btn2.addEventListener("click", ()=>{
     if (localStorage.getItem("local") === null) {
         alert("the localstorage is empty!");
     }
@@ -109,7 +104,8 @@ function searching() {
             }
         }
     }
-}
+}); //searching
+
 
 
 
